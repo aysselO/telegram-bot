a08e266e18d6b45e81ef869755e08bb6e2253a87
Olessya <lesichka2589@gmail.com>
2025-03-09 18:12:03 +0500

    Обновлен токен бота

--- bot.py
+++ bot.py
@@ -1,35 +1,20 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram.types import Message
 
-TOKEN = "7581820794:AAFStUHCR8LROea3cp9e-indtWCnw3jH7QM"
+TOKEN ='7581820794:AAE_sRTIzahZIU_SpRHlZ6xYjUu8MCZOeQQ '
 
-# Включаем логирование
-logging.basicConfig(level=logging.INFO)
-
-# Создаем объекты бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Хэндлер для команды /start
-@dp.message(Command("start"))
-async def start_handler(message: types.Message):
-    await message.answer("Привет! Я твой бот.")
-
-# Хэндлер для команды /help
-@dp.message(Command("help"))
-async def help_handler(message: types.Message):
-    await message.answer("Я могу повторять твои сообщения. Просто напиши мне!")
-
-# Эхо-бот: повторяет все сообщения пользователя
 @dp.message()
-async def echo_handler(message: types.Message):
+async def echo(message: Message):
     await message.answer(message.text)
 
-# Функция запуска бота
 async def main():
+    logging.basicConfig(level=logging.INFO)
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
+if __name__=='__main__':
     asyncio.run(main())
